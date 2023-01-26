84521069f2b23148718b768c9f4dca91d673a8f9
Julian-Barbachano <barbachanojulian@gmail.com>
2023-01-26 12:56:38 -0500

    Commit 2

--- boolean.py
+++ boolean.py
@@ -3,11 +3,10 @@ num1 = int(input("Enter any number: "))
 num2 = int(input("Enter another number: "))
 
 if num1 % num2 == 0:
-    message = "The {var1} is a variable of the {var2}"
-    print(message.format(var1 = "First number", var2 = "Second number"))
+    message = "The number {var1} is a variable of {var2}"
+    print(message.format(var1 = num1, var2 = num2))
 
 else:
-    message = "{var1} is not a variable of {var2}"
-    message = "The {var1} is a variable of the {var2}"
-    print(message.format(var1 = "First number", var2 = "Second number"))
+    message = "The number {var3} is not a variable of the {var4}"
+    print(message.format(var3 = num1, var4 = num2))
 
